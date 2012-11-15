475c8ed13dd22b92181ff541d7398390a0a957c0
rice <yijo1001@gmail.com>
2012-11-15 16:10:26 +0800

    init

---
+++ cookbooks/main/recipes/default.rb
@@ -0,0 +1,16 @@
+package 'git-core'
+package 'zsh'
+
+user node[:user][:name] do
+  username "#{node[:user][:name]}"
+  home "/home/#{node[:user][:name]}"
+  gid 'admin'
+  password "#{node[:user][:password]}"
+  supports manage_home: true
+  shell '/bin/zsh'
+end
+
+template "/home/#{node[:user][:name]}/.zshrc" do
+  source 'zshrc.erb'
+  owner node[:user][:name]
+end
---
+++ solo.rb
@@ -0,0 +1,2 @@
+cookbook_path File.expand_path('./cookbooks', __File__)
+json_attribs File.expand_path("./node.json", __File__)
